7067d99d4b21a814259b09edccf06681c89964a4
waadgasmi <kasmi.waad@gmail.com>
2016-02-27 20:09:24 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,7 @@ library(reshape2)
 
 filename <- "getdata_dataset.zip"
 
+# Step 1:
 ## Download and unzip the dataset:
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
@@ -12,7 +13,7 @@ if (!file.exists("UCI HAR Dataset")) {
 }
 
 
-#Step 1:
+#Step 2:
 # Load activity labels + features
 
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
@@ -20,6 +21,7 @@ activityLabels[,2] <- as.character(activityLabels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
+#Step 2:
 # Extract only the data on mean and standard deviation
 
 FeaturesMean <- grep(".*mean.*|.*std.*", features[,2])
@@ -28,7 +30,7 @@ FeaturesMean.names = gsub('-mean', 'Mean', FeaturesMean.names)
 FeaturesMean.names = gsub('-std', 'Std', FeaturesMean.names)
 FeaturesMean.names <- gsub('[-()]', '', FeaturesMean.names)
 
-#Step 2:
+#Step 3:
 # Load the train and test datasets
 
 train <- read.table("UCI HAR Dataset/train/X_train.txt")[FeaturesMean]
@@ -41,17 +43,20 @@ testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSubjects, testActivities, test)
 
-#Step 3:
+#Step 4:
 # merge datasets and add labels
 
 TidyData <- rbind(train, test)
 colnames(TidyData) <- c("subject", "activity", FeaturesMean.names)
 
+#Step 5:
 # turn activities & subjects into factors
 TidyData$activity <- factor(TidyData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
 TidyData$subject <- as.factor(TidyData$subject)
 
 TidyData.melted <- melt(TidyData, id = c("subject", "activity"))
 TidyData.mean <- dcast(TidyData.melted, subject + activity ~ variable, mean)
-#Step Create the tidy data file. 
+
+#Step 6:
+#Create the tidy data file. 
 write.table(TidyData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
